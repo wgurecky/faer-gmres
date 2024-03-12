7292e138aaed0babefafa306f48eba4aad4c351c
William Gurecky <william.gurecky@gmail.com>
2024-03-12 02:39:00 -0400

    adds license

--- readme.md
+++ readme.md
@@ -2,3 +2,13 @@ About
 =====
 
 GMRES in rust using [faer](https://github.com/sarah-ek/faer-rs).
+
+
+References and Credits
+=======================
+
+This package is an adaptation of GMRES implementation by RLando:
+
+- https://github.com/RLado/GMRES
+- https://crates.io/crates/gmres
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,33 @@
 // https://en.wikipedia.org/wiki/Generalized_minimal_residual_method
 //
 // Uses the Faer library for sparse matricies and sparse solver.
+//
+// Specifically the givens_rotation, apply_givens_rotation and part of the
+// arnoldi implementation is:
+// MIT License
+//
+// Copyright (c) 2023 Ricard Lado
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// https://crates.io/crates/gmres
+//
 use faer::prelude::*;
 use faer::sparse::*;
 use faer::mat;
@@ -17,6 +44,7 @@ fn givens_rotation(v1: f64, v2: f64) -> (f64, f64) {
 }
 
 /// Apply givens rotation to H col
+///
 ///
 fn apply_givens_rotation(h: &mut Vec<f64>, cs: &mut Vec<f64>, sn: &mut Vec<f64>, k: usize) {
     for i in 0..k {
